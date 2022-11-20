681a996721e08f8755f12654ea22f4a9e9361a9c
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-11-19 21:19:46 -0300

    Created an abstract class for athenticable workers

---
+++ src/TesteSistema.java
@@ -0,0 +1,13 @@
+public class TesteSistema {
+    public static void main(String[] args) {
+        Gerente g = new Gerente();
+        g.setSenha(2222);
+
+        Administrador adm = new Administrador();
+        adm.setSenha(123);
+
+        SistemaInterno si = new SistemaInterno();
+        si.autentica(g);
+        si.autentica(adm);
+    }
+}
